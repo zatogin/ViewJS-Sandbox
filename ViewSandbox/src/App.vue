7a32bf5591516f4060f12b8b984583bd07b3e064
Zatohin Aleksandr <zatogin1995@gmail.com>
2020-04-28 13:48:27 +0300

    update

--- ViewSandbox/src/App.vue
+++ ViewSandbox/src/App.vue
@@ -1,20 +1,23 @@
 <template>
-    <div id="app">
-        <Home msg="Hello world!" />
-    </div>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
 </template>
 
 <script>
-    import Home from './components/Home.vue';
-
-    export default {
-        name: 'app',
-        components: {
-            Home
-        }
-    };
+export default {
+  name: 'App'
+}
 </script>
 
 <style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
-
