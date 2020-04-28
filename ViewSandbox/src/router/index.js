0755aa4c5986879818d4470315fa860054626f79
Zatohin Aleksandr <zatogin1995@gmail.com>
2020-04-28 15:49:07 +0300

    Setup complete

--- ViewSandbox/src/router/index.js
+++ ViewSandbox/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/views/HelloWorld'
+import Home from '@/views/Home.vue'
 
 Vue.use(Router)
 
@@ -8,6 +9,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/hello',
       name: 'HelloWorld',
       component: HelloWorld
     }
